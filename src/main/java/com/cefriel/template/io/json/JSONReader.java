c4c834afb73e89d42d08e6347f278bd3c93267f5
Mario Scrocca <mario.scrocca@gmail.com>
2024-05-14 15:03:40 +0200

    fix: JSONReader add config to throw Exception only when required

--- src/main/java/com/cefriel/template/io/json/JSONReader.java
+++ src/main/java/com/cefriel/template/io/json/JSONReader.java
@@ -68,49 +68,51 @@ public class JSONReader implements Reader {
         Set<String> keys = JsonPath.read(queryDoc, "$.paths.keys()");
         List<Map<String, String>> output = new ArrayList<>();
         // config to get results as jsonPaths
-        Configuration conf = Configuration.builder().options(Option.AS_PATH_LIST).build();
+        Configuration conf = Configuration.builder()
+                .options(Option.AS_PATH_LIST, Option.DEFAULT_PATH_LEAF_TO_NULL, Option.SUPPRESS_EXCEPTIONS).build();
         try {
             // Extract paths for each node identified by the iterator
             List<String> results = JsonPath.using(conf).parse(document).read(iterator);
             for (int i=0; i< results.size(); i++)
                 output.add(new HashMap<>());
-            for(String key : keys) {
-                String path = JsonPath.read(queryDoc, "$.paths." + key);
-                Object objects = JsonPath.read(document, iterator + "." + path);
 
-                // objects can either be a list of objects or a single object(string)
-                // if it is a single object then in the output list I have only one item
+            if(!results.isEmpty()) {
+                for(String key : keys) {
+                    String path = JsonPath.read(queryDoc, "$.paths." + key);
+                    Object objects = JsonPath.read(document, iterator + "." + path);
 
-                if (!(objects instanceof JSONArray)) {
-                    String value = objects == null ? "null" : objects.toString();
-                    output.get(0).put(key,value);
-                }
+                    // objects can either be a list of objects or a single object(string)
+                    // if it is a single object then in the output list I have only one item
 
-                else {
-                    JSONArray objectsList = (JSONArray) objects;
-                    // CASE 1, all the nodes identified by the iterator have the key (sub field)
-                    // A single query to extract all values for the key
-                    if (objectsList.size() == results.size()) {
-                        for (int i = 0; i < objectsList.size(); i++) {
-                            String value = objectsList.get(i) == null ? "null" : objectsList.get(i).toString();
-                            output.get(i).put(key, value);
-                        }
-                    }
-                    // CASE 2, not all nodes have the key (sub field)
-                    // For each node a query is executed to get the value for the key (sub field)
-                    // The subquery is composed of the jsonpath for the node and the specific key (sub field)
-                    else {
-                        for (int i = 0; i < results.size(); i++) {
-                            String topPath = results.get(i);
-                            try {
-                                var x = JsonPath.read(document, topPath + "." + path);
-                                var value = x == null ? "null" : x.toString();
+                    if (!(objects instanceof JSONArray)) {
+                        String value = objects == null ? "null" : objects.toString();
+                        output.get(0).put(key, value);
+                    } else {
+                        JSONArray objectsList = (JSONArray) objects;
+                        // CASE 1, all the nodes identified by the iterator have the key (sub field)
+                        // A single query to extract all values for the key
+                        if (objectsList.size() == results.size()) {
+                            for (int i = 0; i < objectsList.size(); i++) {
+                                String value = objectsList.get(i) == null ? "null" : objectsList.get(i).toString();
                                 output.get(i).put(key, value);
-                            } catch (PathNotFoundException pe) {
-                                // what happens when the path is not found? i.e. the item does not have the field the jsonPath is pointing to
-                                // for now we do not put the key in the map
-                                if(verbose) {
-                                    log.warn("PATH NOT FOUND: " + topPath + "." + path);
+                            }
+                        }
+                        // CASE 2, not all nodes have the key (sub field)
+                        // For each node a query is executed to get the value for the key (sub field)
+                        // The subquery is composed of the jsonpath for the node and the specific key (sub field)
+                        else {
+                            for (int i = 0; i < results.size(); i++) {
+                                String topPath = results.get(i);
+                                try {
+                                    var x = JsonPath.read(document, topPath + "." + path);
+                                    var value = x == null ? "null" : x.toString();
+                                    output.get(i).put(key, value);
+                                } catch (PathNotFoundException pe) {
+                                    // what happens when the path is not found? i.e. the item does not have the field the jsonPath is pointing to
+                                    // for now we do not put the key in the map
+                                    if (verbose) {
+                                        log.warn("PATH NOT FOUND: " + topPath + "." + path);
+                                    }
                                 }
                             }
                         }
@@ -118,13 +120,7 @@ public class JSONReader implements Reader {
                 }
             }
         } catch (Exception e) {
-            log.error("ERROR ACCESSING JSON:");
-            Map<String, String> map = new HashMap<>();
-            for(String key : keys) {
-                String path = JsonPath.read(queryDoc, "$.paths." + key);
-                Object object = JsonPath.read(document, iterator + "." + path);
-                log.error(key + "\t" + object.toString());
-            }
+            log.error("Exception while accessing JSON with query: " + query, e);
             throw e;
         }
         return output;
